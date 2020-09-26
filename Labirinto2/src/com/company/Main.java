ae05acb8d636d51aa353dfa2a446405ee4933ea6
richard018 <53762484+richard018@users.noreply.github.com>
2020-09-25 23:05:41 -0300

    Add files via upload

--- Labirinto2/src/com/company/Main.java
+++ Labirinto2/src/com/company/Main.java
@@ -6,6 +6,52 @@ public class Main {
 
     static String labirinto[][] = new String[5][5];
     static Scanner scanner = new Scanner(System.in);
+    static private void moverCima(int x, int y) {
+        int novaPosicao = x - 1;
+
+        labirinto[novaPosicao][y];
+        System.out.println("posição atualizada");
+    }
+
+    static private void moverBaixo(int x, int y) {
+        int novaPosicao = x + 1;
+
+        labirinto[novaPosicao][y];
+        System.out.println("posição atualizada");
+    }
+
+    static private void moverEsquerda(int x, int y) {
+        int novaPosicao = y - 1;
+
+        labirinto[x][novaPosicao];
+        System.out.println("posição atualizada");
+    }
+
+    static private void moverCima(int x, int y) {
+        int novaPosicao = x + 1;
+
+        labirinto[novaPosicao][y];
+        System.out.println("posição atualizada");
+    }
+
+    static private void moverDireita(int x, int y) {
+        int novaPosicao = y + 1;
+
+        labirinto[x][novaPosicao];
+        System.out.println("posição atualizada");
+    }
+
+
+    static private void buscar(int x1, int y1, int x2, int y2) {
+        if (x1 < 0  y1 < 0  x2 > 4 || y2 > 5) {
+            System.out.println("posição inválida");
+        } else {
+            moverCima(x1, y1);
+            moverBaixo(x1, y1);
+            moverEsquerda(x1, y1);
+            moverDireta(x1, y1);
+        }
+    }
 
     public static void main(String[] args) {
         for(int i = 0; i < labirinto.length; i++){
